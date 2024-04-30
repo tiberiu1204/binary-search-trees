a28c94e1ec9a06fdea23ad1dd5fcb5753c0f6e65
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2024-04-30 20:53:51 +0300

    Implemented some specific utility methods.

--- include/avl.h
+++ include/avl.h
@@ -19,7 +19,58 @@ public:
 private:
     using Node = typename BinarySearchTree<T>::Node;
     using restricted_iterator = typename BinarySearchTree<T>::restricted_iterator;
+private:
+    int left_subtree_height(Node *node);
+    int right_subtree_height(Node *node);
+    int balance_factor(Node *node);
+    Node *left_rotete(Node *node);
+    Node *right_rotate(Node *node);
 };
+
+template<typename T>
+AVLTree<T>::Node *AVLTree<T>::left_rotete(AVLTree::Node *node)
+{
+    Node *right = node->right();
+    node->right = right->left();
+    right->left = node;
+    return right;
+}
+
+template<typename T>
+AVLTree<T>::Node *AVLTree<T>::right_rotate(AVLTree::Node *node)
+{
+    Node *left = node->left();
+    node->left = left->right();
+    left->right = node;
+    return left;
+}
+
+template<typename T>
+int AVLTree<T>::left_subtree_height(AVLTree::Node *node)
+{
+    Node &left = node->left();
+    if (!node->has_left()) return 0;
+    else {
+        return std::max(left_subtree_height(left), right_subtree_height(left)) + 1;
+    }
+}
+
+template<typename T>
+int AVLTree<T>::right_subtree_height(AVLTree::Node *node)
+{
+    Node &right = node->right();
+    if (!node->has_left()) return 0;
+    else {
+        return std::max(left_subtree_height(right), right_subtree_height(right)) + 1;
+    }
+}
+
+template<typename T>
+int AVLTree<T>::balance_factor(AVLTree::Node *node)
+{
+    return left_subtree_height(node) - right_subtree_height(node);
+}
+
 template<typename T>
 void AVLTree<T>::remove(const T &value)
 {
