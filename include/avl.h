18135edf22321e63f1468e679f47c4c46f03f90f
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2024-05-01 03:51:23 +0300

    Finished the AVL tree, currently segfaults on insert.

--- include/avl.h
+++ include/avl.h
@@ -7,6 +7,7 @@
 
 
 #include "bst.h"
+#include <iostream>
 
 template <typename T>
 class AVLTree : public BinarySearchTree<T> {
@@ -20,53 +21,80 @@ private:
     using Node = typename BinarySearchTree<T>::Node;
     using restricted_iterator = typename BinarySearchTree<T>::restricted_iterator;
 private:
-    int left_subtree_height(Node *node);
-    int right_subtree_height(Node *node);
-    int balance_factor(Node *node);
-    Node *left_rotete(Node *node);
-    Node *right_rotate(Node *node);
+    int left_subtree_height(Node &node);
+
+    int right_subtree_height(Node &node);
+
+    int balance_factor(Node &node);
+
+    Node &left_rotate(Node &node);
+
+    Node &right_rotate(Node &node);
+
+    void balance(Node &node);
 };
 
 template<typename T>
-AVLTree<T>::Node *AVLTree<T>::left_rotete(AVLTree::Node *node)
+void AVLTree<T>::balance(AVLTree::Node &node)
 {
-    Node *right = node->right();
-    node->right = right->left();
-    right->left = node;
-    return right;
+    Node *node_ptr = &node;
+    while (node_ptr->get_parent_index() > 0) {
+        if (balance_factor(*node_ptr) >= 2 and balance_factor(node_ptr->left()) >= 0)   // left - left
+            *node_ptr = right_rotate(*node_ptr);
+
+        else if (balance_factor(*node_ptr) >= 2) {  // left - right
+            node_ptr->left() = left_rotate(node_ptr->left());
+            *node_ptr = right_rotate(*node_ptr);
+        } else if (balance_factor(*node_ptr) <= -2 and balance_factor(node_ptr->right()) <= 0)  // right - right
+            *node_ptr = left_rotate(*node_ptr);
+
+        else if (balance_factor(*node_ptr) <= -2) {  // right - left
+            node_ptr->right() = right_rotate(node_ptr->right());
+            *node_ptr = left_rotate(*node_ptr);
+        } else node_ptr = &node_ptr->parent();
+    }
 }
 
 template<typename T>
-AVLTree<T>::Node *AVLTree<T>::right_rotate(AVLTree::Node *node)
+AVLTree<T>::Node &AVLTree<T>::left_rotate(AVLTree::Node &node)
 {
-    Node *left = node->left();
-    node->left = left->right();
-    left->right = node;
-    return left;
+    Node &right_child = node.right();
+    node.set_right_index(right_child.get_left_index());
+    right_child.set_left_index(node.get_node_index());
+    return right_child;
 }
 
 template<typename T>
-int AVLTree<T>::left_subtree_height(AVLTree::Node *node)
+AVLTree<T>::Node &AVLTree<T>::right_rotate(AVLTree::Node &node)
 {
-    Node &left = node->left();
-    if (!node->has_left()) return 0;
+    Node &left_child = node.left();
+    node.set_left_index(left_child.get_right_index());
+    left_child.set_right_index(node.get_node_index());
+    return left_child;
+}
+
+template<typename T>
+int AVLTree<T>::left_subtree_height(AVLTree::Node &node)
+{
+    Node &left = node.left();
+    if (!node.has_left()) return 0;
     else {
         return std::max(left_subtree_height(left), right_subtree_height(left)) + 1;
     }
 }
 
 template<typename T>
-int AVLTree<T>::right_subtree_height(AVLTree::Node *node)
+int AVLTree<T>::right_subtree_height(AVLTree::Node &node)
 {
-    Node &right = node->right();
-    if (!node->has_left()) return 0;
+    Node &right = node.right();
+    if (!node.has_right()) return 0;
     else {
         return std::max(left_subtree_height(right), right_subtree_height(right)) + 1;
     }
 }
 
 template<typename T>
-int AVLTree<T>::balance_factor(AVLTree::Node *node)
+int AVLTree<T>::balance_factor(AVLTree::Node &node)
 {
     return left_subtree_height(node) - right_subtree_height(node);
 }
@@ -74,11 +102,62 @@ int AVLTree<T>::balance_factor(AVLTree::Node *node)
 template<typename T>
 void AVLTree<T>::remove(const T &value)
 {
+    restricted_iterator it = this->lookup(value);
+    if (it == this->end()) return;
+    Node &node = it.get_node();
+    if (!node.has_left() && !node.has_right()) {
+        if (node.is_left_sibling()) node.parent().set_left_index(0);
+        else node.parent().set_right_index(0);
+        this->pop(node);
+    } else if (!node.has_left()) {
+        node.set_value(node.right().get_value());
+        this->pop(node.right());
+        node.set_right_index(0);
+    } else if (!node.has_right()) {
+        node.set_value(node.left().get_value());
+        this->pop(node.left());
+        node.set_left_index(0);
+    } else {
+        it++;
+        Node &next = it.get_node();
+        node.set_value(next.get_value());
+        if (next.is_left_sibling()) next.parent().set_left_index(0);
+        else next.parent().set_right_index(0);
+        this->pop(next);
+    }
+    balance(node);
 }
 
 template<typename T>
 AVLTree<T>::iterator AVLTree<T>::insert(const T &value)
 {
+    if (this->empty()) {
+        this->emplace(value);
+        return this->begin();
+    }
+
+    Node *node = &this->root();
+
+    while (true) {
+        if (value < node->get_value()) {
+            if (!node->has_left()) {
+                node->set_left_index(this->next_index());
+                this->emplace(value, node->get_node_index());
+                break;
+            }
+            node = &node->left();
+        } else if (value > node->get_value()) {
+            if (!node->has_right()) {
+                node->set_right_index(this->next_index());
+                this->emplace(value, node->get_node_index());
+                break;
+            }
+            node = &node->right();
+        }
+    }
+    Node &new_node = this->back();
+    balance(new_node);
+    return this->find(value);
 }
 
 template<typename T>
@@ -88,6 +167,33 @@ AVLTree<T>::AVLTree(std::vector<T> values)
         this->insert(value);
     }
 }
+
+template
+class AVLTree<int>;
+
+template
+class AVLTree<float>;
+
+template
+class AVLTree<double>;
+
+template
+class AVLTree<unsigned int>;
+
+template
+class AVLTree<unsigned long long>;
+
+template
+class AVLTree<long long>;
+
+template
+class AVLTree<short>;
+
+template
+class AVLTree<char>;
+
+template
+class AVLTree<unsigned char>;
 
 
 #endif //BINARY_SEARCH_TREES_AVL_H
--- main.cpp
+++ main.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
-#include "scapegoat.h"
+#include "avl.h"
 
 int main() {
     std::vector<int> v = {2, 4, 1, 9, 0, 5, 10, 11, 15, 8, 20, 16, 50, 23, 12, 43, 60, 70, 123, 534, 124, 6545};
-    ScapegoatTree<int> scapegoat_tree(v, 0.5);
-    for(auto elem : scapegoat_tree) {
+    AVLTree<int> avl(v);
+    for (auto elem: avl) {
         std::cout<<elem<<" ";
     }
     std::cout<<"\n";
-    scapegoat_tree.remove(6545);
-    for(auto elem : scapegoat_tree) {
+    avl.remove(6545);
+    for (auto elem: avl) {
         std::cout<<elem<<" ";
     }
     std::cout<<"\n";
-    scapegoat_tree.remove(0);
-    for(auto elem : scapegoat_tree) {
+    avl.remove(0);
+    for (auto elem: avl) {
         std::cout<<elem<<" ";
     }
     std::cout<<"\n";
-    scapegoat_tree.remove(20);
-    for(auto elem : scapegoat_tree) {
+    avl.remove(20);
+    for (auto elem: avl) {
         std::cout<<elem<<" ";
     }
     std::cout<<"\n";
-    scapegoat_tree.remove(15);
-    for(auto elem : scapegoat_tree) {
+    avl.remove(15);
+    for (auto elem: avl) {
         std::cout<<elem<<" ";
     }
     std::cout<<"\n";
-    scapegoat_tree.remove(11);
-    for(auto elem : scapegoat_tree) {
+    avl.remove(11);
+    for (auto elem: avl) {
         std::cout<<elem<<" ";
     }
     std::cout<<"\n";
-    std::cout<<*(scapegoat_tree.find(50) + 2);
+    std::cout << *(avl.find(50) + 2);
 }
