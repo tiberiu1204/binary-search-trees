d2dc0416340cb218c8bdd306dc21f6c1dc5df86e
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2024-05-01 19:55:01 +0300

    Test environment.

--- main.cpp
+++ main.cpp
@@ -1,37 +1,83 @@
 #include <iostream>
+#include <chrono>
+#include <numeric>
+#include <random>
 #include "avl.h"
+#include "scapegoat.h"
+
+std::vector<std::vector<int>> prepare_vectors(const std::vector<int> &sizes)
+{
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::vector<std::vector<int>> vectors;
+    for (auto &size: sizes) {
+        vectors.emplace_back(size);
+        auto &vector = vectors.back();
+        std::iota(std::begin(vector), std::end(vector), 0);
+        std::shuffle(std::begin(vector), std::end(vector), g);
+    }
+    return vectors;
+}
+
+void test_trees(const std::vector<std::vector<int>> &vectors)
+{
+    using namespace std::chrono;
+
+    for (const auto &vector: vectors) {
+        auto start = high_resolution_clock::now();
+        AVLTree<int> avl_tree(vector);
+        auto end = high_resolution_clock::now();
+        auto time = duration_cast<milliseconds>(end - start);
+        std::cout << "AVL tree insertion time for " << vector.size() << " elements: " << time << std::endl;
+
+        start = high_resolution_clock::now();
+        ScapegoatTree<int> sg_tree(vector);
+        end = high_resolution_clock::now();
+        time = duration_cast<milliseconds>(end - start);
+        std::cout << "Scapegoat tree insertion time for " << vector.size() << " elements: " << time << std::endl;
+
+        size_t qty = vector.size() / 5;
+
+        start = high_resolution_clock::now();
+        for (size_t i = vector.size() - 1; i >= vector.size() - qty - 1; --i) {
+            avl_tree.find(vector.at(i));
+        }
+        end = high_resolution_clock::now();
+        time = duration_cast<milliseconds>(end - start);
+        std::cout << "AVL tree look-up time for " << vector.size() << " elements, " << qty << " look-ups: " << time
+                  << std::endl;
+
+        start = high_resolution_clock::now();
+        for (size_t i = vector.size() - 1; i >= vector.size() - qty - 1; --i) {
+            sg_tree.find(vector.at(i));
+        }
+        end = high_resolution_clock::now();
+        time = duration_cast<milliseconds>(end - start);
+        std::cout << "Scapegoat tree look-up time for " << vector.size() << " elements, " << qty << " look-ups: "
+                  << time << std::endl;
+
+        start = high_resolution_clock::now();
+        for (size_t i = vector.size() - 1; i >= vector.size() - qty - 1; --i) {
+            avl_tree.remove(vector.at(i));
+        }
+        end = high_resolution_clock::now();
+        time = duration_cast<milliseconds>(end - start);
+        std::cout << "AVL tree removal time for " << vector.size() << " elements, " << qty << " removals: " << time
+                  << std::endl;
+
+        start = high_resolution_clock::now();
+        for (size_t i = vector.size() - 1; i >= vector.size() - qty - 1; --i) {
+            sg_tree.remove(vector.at(i));
+        }
+        end = high_resolution_clock::now();
+        time = duration_cast<milliseconds>(end - start);
+        std::cout << "Scapegoat tree removal time for " << vector.size() << " elements, " << qty << " removals: "
+                  << time << std::endl;
+    }
+}
 
 int main() {
-    std::vector<int> v = {2, 4, 1, 9, 0, 5, 10, 11, 15, 8, 20, 16, 50, 23, 12, 43, 60, 70, 123, 534, 124, 6545};
-    AVLTree<int> avl(v);
-    for (auto elem: avl) {
-        std::cout<<elem<<" ";
-    }
-    std::cout<<"\n";
-    avl.remove(6545);
-    for (auto elem: avl) {
-        std::cout<<elem<<" ";
-    }
-    std::cout<<"\n";
-    avl.remove(0);
-    for (auto elem: avl) {
-        std::cout<<elem<<" ";
-    }
-    std::cout<<"\n";
-    avl.remove(20);
-    for (auto elem: avl) {
-        std::cout<<elem<<" ";
-    }
-    std::cout<<"\n";
-    avl.remove(15);
-    for (auto elem: avl) {
-        std::cout<<elem<<" ";
-    }
-    std::cout<<"\n";
-    avl.remove(11);
-    for (auto elem: avl) {
-        std::cout<<elem<<" ";
-    }
-    std::cout<<"\n";
-    std::cout << *(avl.find(50) + 2);
+    std::vector<int> sizes = {10000};
+    auto test_vectors = prepare_vectors(sizes);
+    test_trees(test_vectors);
 }
